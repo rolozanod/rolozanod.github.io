ee12013ceddc75257500ad8b08ae3c7c7b40f991
rolozanod <rolozanod@users.noreply.github.com>
2022-03-07 22:08:24 +0000

    deploy: rolozanod/rolozanod.github.io_source@c547979f694dba72b2c3d49a6af6dac94b96ab55

--- js/webcam.js
+++ js/webcam.js
@@ -60,7 +60,10 @@ class Webcam {
   
   
 	cropImage(img) {
-	  const size = Math.min(img.shape[0], img.shape[1]);
+	  /**
+	   * const size = Math.min(img.shape[0], img.shape[1]);
+	  */
+	  const size = 224;
 	  const centerHeight = img.shape[0] / 2;
 	  const beginHeight = centerHeight - (size / 2);
 	  const centerWidth = img.shape[1] / 2;
